8414afd0bda7f784d74f3a135f4224d178f3b8e5
Ninjego <ninjego1@hotmail.com>
2022-07-15 22:50:00 +0200

    Setup client

---
+++ src/main/java/me/ninjego/whoreaxe/WhoreAxe.java
@@ -0,0 +1,15 @@
+package me.ninjego.whoreaxe;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WhoreAxe implements ModInitializer {
+	public static final String MOD_ID = "whoreaxe";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/me/ninjego/whoreaxe/WhoreAxeClient.java
@@ -0,0 +1,11 @@
+package me.ninjego.whoreaxe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WhoreAxeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
