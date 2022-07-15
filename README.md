424bd0307036bc2b11f0f483ff427e8d42473aaf
Ninjego <ninjego1@hotmail.com>
2022-07-15 23:42:04 +0200

    Empty project, starting work

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 
 Whoreaxe is a Minecraft mod for utilization by Ninjego
 
+##Credits
+
+Fabric Team for Fabric
+
 ## License
 
-This project is under the CC0 license
+This project is licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html).
+
+_If you have any other question, add me on discord_ **Ninjego#9126**
--- src/main/java/me/ninjego/whoreaxe/WhoreAxe.java
+++ src/main/java/me/ninjego/whoreaxe/WhoreAxe.java
@@ -1,15 +1,16 @@
 package me.ninjego.whoreaxe;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class WhoreAxe implements ModInitializer {
+public class WhoreAxe implements ClientModInitializer {
 	public static final String MOD_ID = "whoreaxe";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 
 	}
 }
